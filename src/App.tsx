cf7ab650506da8be16ece8baf15f4841b250c266
Maciej Hełmecki <mhelmecki@gmail.com>
2022-01-12 01:26:14 +0100

    Add Firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,19 @@ import Header from './components/Header/Header';
 import { MainContent } from './components/MainContent/MainContent';
 import { Logo } from './icons/Logo';
 
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDKCpvKwUNcDnjGarrM1MruzrF96xu_2bc',
+  authDomain: 'video-call-diagnostics.firebaseapp.com',
+  projectId: 'video-call-diagnostics',
+  storageBucket: 'video-call-diagnostics.appspot.com',
+  messagingSenderId: '264571240401',
+  appId: '1:264571240401:web:1dfcc2a528dab0ab98eaf0',
+  measurementId: 'G-HJB60FW93N',
+};
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     appContainer: {
@@ -22,6 +35,8 @@ const useStyles = makeStyles((theme: Theme) =>
 
 function App() {
   const classes = useStyles();
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
   return (
     <AppStateProvider>
